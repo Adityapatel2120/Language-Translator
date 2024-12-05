a03b26250b080915529f287d5f276e5e1529dfe1
Adityapatel2120 <135309934+Adityapatel2120@users.noreply.github.com>
2024-12-05 21:50:20 +0530

    initial-translation

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-var username = prompt("Enter username:")
-alert("Welcome Mr."+username)
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+console.log(txtInput)
+var serverurl="https://api.funtranslations.com/translate/pirate.json"
+
+function getserverurl(input){
+    return serverurl + "?" + "text=" + input
+}
+
+function clickHandler(){
+    var inputtxt= txtInput.value;
+
+    fetch(getserverurl(inputtxt))
+        .then(response => response.json())
+        .then(json => {
+            var TranslatedText=json.contents.translated;
+            outputDiv.innerText=TranslatedText;
+            })
+};
+
+btnTranslate.addEventListener("click",clickHandler)
